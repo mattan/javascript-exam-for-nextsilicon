91e862c50bcc3ac9e58d09146baf9c424008506d
מתן ליבוביץ <mattan.friends+github@gmail.com>
2022-08-10 21:43:31 +0300

    Temp version without the css

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("hello world");
+class MacMenu extends HTMLElement {
+  
+    constructor(imgs=[]) {
+        super();
+        const shadow = this.attachShadow({mode: 'open'});
+        const div = document.createElement('div');
+        shadow.appendChild(div);
+        div.setAttribute('id','main');
+        const ul = document.createElement('ul');
+        div.appendChild(ul);
+
+        for (let img of Database.getImgs()){
+            let li = document.createElement('li');
+            ul.appendChild(li);
+            let span = document.createElement('span');
+            li.appendChild(span);
+            span.textContent = img.title;
+
+            let href = document.createElement('a');
+            li.appendChild(href);
+            href.setAttribute('href','#');
+            let imgDiv = document.createElement('img');
+            href.appendChild(imgDiv);
+            imgDiv.setAttribute('src',img.src);
+        }
+            
+    }
+  }
+
+  class Database {
+    static getImgs(){
+        return [
+            {src:"Mac-Address-Book-icon.png",title:"Address Book"},
+            {src:"Mac-App-Store-icon.png",title:"App Store"},
+            {src:"chrome_ico.png",title:"Chrome"},
+            {src:"firefox.png",title:"Firefox"},
+            {src:"Mac-icon.png",title:"Mac"},
+            {src:"mac-icon2.png",title:"Icon"},
+            {src:"MetroUI-Apps-Mac-iChat-icon.png",title:"Chat"},
+            {src:"apple-icon.png",title:"Apple"}
+        ]
+    }
+  }
+
+  customElements.define('mac-menu', MacMenu);
