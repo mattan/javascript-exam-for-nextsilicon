908ba6b18bdf1f6f0546c67756259269484ec87a
מתן ליבוביץ <mattan.friends+github@gmail.com>
2022-08-12 01:11:57 +0300

    Moving from predefine imgs to "mac-icon" tag

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ class MacMenu extends HTMLElement {
     constructor(imgs=[]) {
         super();
         const shadow = this.attachShadow({mode: 'open'});
+        shadow.appendChild(this.#getStyle());
         const div = document.createElement('div');
         shadow.appendChild(div);
         div.setAttribute('id','main');
         const ul = document.createElement('ul');
         div.appendChild(ul);
 
-        for (let img of Database.getImgs()){
-            this.#addImg(ul,img);
+        for (let macIconDiv of this.children){
+            this.#addImg(ul,this.#divToImg(macIconDiv));
         }
             
     }
@@ -32,21 +33,82 @@ class MacMenu extends HTMLElement {
         imgDiv.setAttribute('src',img.src);
     }
 
-  }
-
-  class Database {
-    static getImgs(){
-        return [
-            {src:"Mac-Address-Book-icon.png",title:"Address Book"},
-            {src:"Mac-App-Store-icon.png",title:"App Store"},
-            {src:"chrome_ico.png",title:"Chrome"},
-            {src:"firefox.png",title:"Firefox"},
-            {src:"Mac-icon.png",title:"Mac"},
-            {src:"mac-icon2.png",title:"Icon"},
-            {src:"MetroUI-Apps-Mac-iChat-icon.png",title:"Chat"},
-            {src:"apple-icon.png",title:"Apple"}
-        ]
+    #divToImg(macIconDiv){
+        return {
+            src:macIconDiv.getAttribute("src"),
+            title:macIconDiv.innerText
+        }
     }
+
+    #getStyle(){
+        let style = document.createElement('style');
+
+        style.textContent = `
+            body { font-family:arial; margin: 0; background: #052325; }
+
+
+            .wrapper {
+                width: 100%;
+                background-size: 100%;
+                padding: 0 !important;
+            }
+            p { color: #fff;}
+            
+            ul {
+                padding: 0;
+            }
+            
+            
+            #main {
+                position: fixed;
+                bottom: 0;
+                text-align: center;
+                right: 20%;
+                left: 20%;
+                width: 60%;
+                background: rgba(255,255,255,0.2);
+                border-radius: 10px 10px 0 0;
+            }
+            #main li {
+                list-style-type: none;
+                display: inline-block;
+                position: relative;
+            }
+            
+            #main li img {
+            width: 64px;
+            height: 64px;
+            transition: all 0.3s;
+            -webkit-transform-origin: 50% 100%;
+            }
+            #main li:hover img { 
+            -webkit-transform: scale(2);
+            margin: 0 2em;
+            }
+            #main li:hover + li img,
+            #main li.prev img {
+            -webkit-transform: scale(1.5);
+            margin: 0 1.5em;
+            }
+            
+            #main li span {
+                display: none;
+                position: absolute;
+                bottom: 140px;
+                left: 0;
+                width: 100%;
+                background-color: rgba(0,0,0,0.75);
+                padding: 4px 0;
+                border-radius: 12px;
+            }
+            #main li:hover span {
+                display: block;
+                color: #fff;
+            }
+        `
+        return style;
+    }
+
   }
 
   customElements.define('mac-menu', MacMenu);
